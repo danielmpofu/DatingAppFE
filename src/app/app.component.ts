bf31eff27e620c66c55f5af0e48effb67d663d1d
Daniel T Mpofu <danielmpofu@hotmail.com>
2023-07-21 11:02:25 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title: String = 'DatingAppFE';
+  users: any;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get("http://localhost:5108/api/v1/Users")
+      .subscribe({
+        next: (response) => {
+          this.users = response;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log("request has completed");
+        }
+      })
+  }
+
+}
