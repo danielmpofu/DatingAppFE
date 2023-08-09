3bab4c845d51101e50920576d63b4e8cc1ea5619
Daniel T Mpofu <50142620+danielmpofu@users.noreply.github.com>
2023-08-09 09:10:45 +0200

    merged with form validation branch

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -14,38 +14,26 @@ export class RegisterComponent implements OnInit {
   formData: any = {}
   formValidators = [Validators.required];
 
+
   initializeForm() {
     this.registerForm = new FormGroup({
       username: new FormControl('',
         this.formValidators),
       password: new FormControl('',
         [Validators.required, Validators.min(4), Validators.max(8)]),
-      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')]),
-      phone: new FormControl('',this.formValidators),
-      email: new FormControl('',this.formValidators),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+      phone: new FormControl('', this.formValidators),
+      email: new FormControl('', this.formValidators),
     });
-
-    this.registerForm.controls('password').valueChanges().subscribe()
+    this.registerForm
+      .controls["password"]
+      .valueChanges
+      .subscribe({
+        next: () => {
+          this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+        }
+      });
   }
-
-    initializeForm() {
-        this.registerForm = new FormGroup({
-            username: new FormControl(),
-            password: new FormControl(),
-            confirmPassword: new FormControl(),
-            phone: new FormControl(),
-            email: new FormControl(),
-        });
-
-        this.registerForm
-            .controls["password"]
-            .valueChanges
-            .subscribe({
-                next: () => {
-                    this.registerForm.controls['confirmPassword'].updateValueAndValidity()
-                }
-            });
-    }
 
   constructor(private accountService: AccountService) {
   }
@@ -54,15 +42,15 @@ export class RegisterComponent implements OnInit {
     this.initializeForm();
   }
 
-  matchValues(matchTo: string) :ValidatorFn {
-   return (control:AbstractControl) => {
-     if(control) {
-       // @ts-ignore
-       return control.value === control.parent?.get(matchTo).value ? null : {notMatching: true};
-     }else{
-       return  null;
-     }
-   }
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      if (control) {
+        // @ts-ignore
+        return control.value === control.parent?.get(matchTo).value ? null : {notMatching: true};
+      } else {
+        return null;
+      }
+    }
   }
 
   doRegister(): void {
